dea3c4618c70c55b1553f4c5f34f36731e08a80e
rohan <rsureshkumar@empyra.com>
2019-04-19 22:35:54 +0530

    Adding history text

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,7 +9,8 @@ class Calculator extends React.Component {
             variableOne: 0,
             variableTwo: 0,
             action:"", 
-            clear: false
+            clear: false,
+            historyText:""
         }
         this.addToDisplay = this.addToDisplay.bind(this)
         this.clearDisplay = this.clearDisplay.bind(this)
@@ -51,7 +52,8 @@ class Calculator extends React.Component {
             return{
             variableOne: prevState.displayText,
             displayText : "",
-            action: "gcd"
+            action: "gcd",
+            historyText: prevState.displayText + " GCD "
             }
         })
     }
@@ -61,12 +63,15 @@ class Calculator extends React.Component {
             return{
             variableOne: prevState.displayText,
             displayText : "",
-            action: "lcm"
+            action: "lcm",
+            historyText: prevState.displayText + " LCM "
             }
         })
     }
 
     calculateGCD(variableOne,variableTwo) {
+        if (variableOne === 0 || variableTwo === 0)
+            return 0
         let result = 1
         if (variableOne % variableTwo === 0) {
             result = variableTwo
@@ -115,60 +120,72 @@ class Calculator extends React.Component {
 
     result(event) {
         this.setState((prevState) => {
-            if(prevState.action === "gcd"){
-                let variableOne = parseInt(prevState.variableOne)
-                let variableTwo = parseInt(prevState.displayText)   
+            let variableOne = parseInt(prevState.variableOne)
+            if (prevState.displayText === "")
+                prevState.displayText = 0
+                let variableTwo = parseInt(prevState.displayText)  
+                if(prevState.action === "gcd"){
                 let result = this.calculateGCD(variableOne,variableTwo)
                 return{
                     variableOne: 0,
                     displayText : result,
                     clear: true,
-                    action: ""
+                    action: "",
+                    historyText: prevState.historyText + " " + prevState.displayText + " = " + result
                     }
 
-            }
-            else if(prevState.action == "lcm"){
-                let variableOne = parseInt(prevState.variableOne)
-                let variableTwo = parseInt(prevState.displayText)   
+                }
+                else if(prevState.action == "lcm"){      
                 let result = this.calculateLCM(variableOne,variableTwo)
                 return{
                     variableOne: 0,
                     displayText : result,
                     clear: true,
-                    action: ""
+                    action: "",
+                    historyText: prevState.historyText + " " + prevState.displayText + " = " + result
                     }
-            }
+                }
         })
     }
 
     render() {
         return (
             <div className="calculator" style={{marginTop: 5 +'em', marginLeft: 20 + 'em'}}>
-                <input type="text" value={this.state.displayText}
+            
+                <input type="text" value={this.state.displayText} className="display"
                  readOnly></input>
                  <br/>
-                <button onClick={this.addToDisplay}>1</button>
-                <button onClick={this.addToDisplay}>2</button>
-                <button onClick={this.addToDisplay}>3</button>
-                <button onClick={this.addToDisplay}>4</button>
-                <button onClick={this.addToDisplay}>5</button>
-                <button onClick={this.clearDisplay}>C</button>
-                <br/>
-                <button onClick={this.addToDisplay}>6</button>
-                <button onClick={this.addToDisplay}>7</button>
-                <button onClick={this.addToDisplay}>8</button>
-                <button onClick={this.addToDisplay}>9</button>
-                <button onClick={this.addToDisplay}>0</button>
-                <button onClick={this.deleteLastChar}> &#8592; </button>
-                <br/>
-                
-                
-                <button onClick={this.gcd}>GCD</button>
-                <button onClick={this.lcm}>LCM</button>{
-                //<button onClick={this.lcm}>Prime?</button>
-                //<button onClick={this.lcm}>Perfect Square?</button>
-                }
-                <button onClick={this.result}>=</button>
+                 <div className="inputBoxes">
+                    <div className="row">
+                        <button onClick={this.addToDisplay}>1</button>
+                        <button onClick={this.addToDisplay}>2</button>
+                        <button onClick={this.addToDisplay}>3</button>
+                        <button onClick={this.addToDisplay}>4</button>
+                        <button onClick={this.addToDisplay}>5</button>
+                        <button onClick={this.clearDisplay}>C</button>
+                    </div>
+                    <div className="row">
+                    <button onClick={this.addToDisplay}>6</button>
+                    <button onClick={this.addToDisplay}>7</button>
+                    <button onClick={this.addToDisplay}>8</button>
+                    <button onClick={this.addToDisplay}>9</button>
+                    <button onClick={this.addToDisplay}>0</button>
+                    <button onClick={this.deleteLastChar}> &#8592; </button>
+                    </div>
+                    
+                    <div className="row">
+                    <button onClick={this.gcd}>GCD</button>
+                    <button onClick={this.lcm}>LCM</button>{
+                    //<button onClick={this.lcm}>Prime?</button>
+                    //<button onClick={this.lcm}>Perfect Square?</button>
+                    }
+                    <button onClick={this.result} style={{width:26.5+'%', textAlign:'center'}}>=</button>
+                    </div>
+                  
+                </div>
+                <input type="text" value={this.state.historyText}
+        
+                     readOnly className="history"></input>
             </div>
         )
     }
