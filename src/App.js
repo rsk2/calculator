3fb7a1cd3ec7e686fce48d32027d13c9844427d1
Rohan <rsureshkumar@empyra.com>
2019-11-13 17:43:28 +0530

    Removing unnecessary lines of code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header.js'
-import Calculator from './components/Calculator.js'
-import Footer from './components/Footer.js'
+import Calculator from './components/Calculator.js';
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
 
 class App extends Component {
   render() {
