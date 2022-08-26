14c2c19cd9a93aba6f846c9d1c1fd1de6f479840
Rohan <beingrohan24.7@gmail.com>
2022-08-26 17:50:31 +0530

    Issue 4: Build folder update

---
+++ build/precache-manifest.aae8d138f457635bae6c14023d067ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2355216bec896b6767",
+    "url": "/calculator/static/css/main.1ad68a1d.chunk.css"
+  },
+  {
+    "revision": "ec2355216bec896b6767",
+    "url": "/calculator/static/js/main.83a83add.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "a64541ec99099578c538",
+    "url": "/calculator/static/js/2.5016a68c.chunk.js"
+  },
+  {
+    "revision": "3ddf49799c84737d970024a7a073530b",
+    "url": "/calculator/index.html"
+  }
+];
